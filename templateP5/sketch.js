c16e590e18896260d766e8706255ea6f419c5db8
ishaangupta2 <143498568+ishaangupta2@users.noreply.github.com>
2023-10-19 04:53:26 +0800

    Final Project Code

--- templateP5/sketch.js
+++ templateP5/sketch.js
@@ -1,67 +1,275 @@
-console.log("js is linked!");
-
 let panic = 0;
-let timer = 0;
-let reds = [];
+let timer = 30;
+
+let a = 200;
+let b = 200;
+
+let diam = 0;
+
+let wave;
 
 function setup(){
     let cnv = createCanvas(600, 400);
     cnv.parent("canvasWrapper");
+    angleMode(DEGREES);
+
 }
 
-function draw(){
- 
-  // Draw sky
-  background(135, 206, 235);
-
-  // Draw sun
-  fill(255, 255, 0);
-  ellipse(100, 100, 80, 80);
-
-  // Draw mountains
-  fill(139, 69, 19);
-  triangle(50, height - 50, width / 2, height / 2 - 50, width - 50, height - 50);
+function draw() {
   
-  if (panic < 100) {
-    background(200, 200, 100);
-    drawGoat(mouseX, mouseY);
-    if (keyIsPressed) {
+  if (panic < 500) {
+ background(159, 189, 237);
+  
+  // sky
+  for(let i=0; i<=height; i++){
+    stroke(255-i,255-i,255-i);
+    line(0,200-i,width,200-i);
+  }
+  
+  // sun
+  noStroke();
+  fill(250,186, 3);
+  ellipse(200,100, 70, 70);
+  
+  // mountains
+  fill(77, 61, 18);
+  triangle(38, 208, 102, 63, 145, 200);
+  triangle(175, 208, 205, 105, 250, 200);
+  triangle(235, 208, 270, 63, 290, 200);
+  triangle(300, 208, 320, 105, 340, 200);  
+    
+  // cloud
+    for(let c=0; c<6; c++){
+    fill(255, 255, 255);
+    ellipse(50 + (5 * c), 100, 40 + c, 20);
+    ellipse(100 + (4 * c), 120, 40 + c, 20);
+    ellipse(175 + (5 * c), 110, 40 + c, 20);
+    ellipse(432 + (5 * c), 110, 40 + c, 20);
+    }
+    
+  // pasture
+  noStroke();
+  fill(0,100,0);
+  rect(0,200,width,200);
+  
+  // trees
+  fill(67, 51, 8); 
+  rect(320,194,10,20); 
+  fill(200, 255, 100);
+  ellipse(326,172,54,20);
+  ellipse(324,192,30,20);
+  ellipse(330,182,30,20);
+
+  push();
+    translate(-40,-40);
+    scale(1.5);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326, 192, 15, 20);
+    ellipse(324,172,30,20);
+    ellipse(330,182,20,20);
+  pop();
+    
+  push();
+    translate(-80,-80);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,20,20);
+    ellipse(324,172,30,20);
+    ellipse(330,182,20,20);
+  pop();
+    
+  push();
+    translate(-400,-120);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,16,20);
+    ellipse(324,172,25,20);
+    ellipse(330,182,8,20);
+  pop();
+    
+  push();
+    translate(-550,-80);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,20,20);
+    ellipse(324,172,15,20);
+    ellipse(330,182,15,20);
+  pop();
+    
+    drawGoat(a, b);
+    
+      if (keyCode === LEFT_ARROW) {
+      a = a - 3;
       panic++;
-    }
-    if (frameCount % 60 == 0) {
-      timer++;
-    }
-    if (timer == 15) {
-      panic = 100;
-    }
+      } else if (keyCode === RIGHT_ARROW) {
+      a = a + 3;
+      panic++;
+      } else if (keyCode === UP_ARROW) {
+      b = b - 3;
+      panic++;
+      } else if (keyCode === DOWN_ARROW) {
+      b = b + 3;
+      panic++;
+      }
+    
+    text('Use the arrow keys to control the goat.', width/2, height*0.10)
+      if (a > width && a < 0) {
+        a *= -1;
+      }
+      if (b > height && b < 200) {
+        b *= -1;
+      }
+    
+    //if (frameCount % 60 == 0) {
+      //timer++;
+    //}
+    //if (timer == 15) {
+      //panic = 100;
+    //}
   } else {
     background(0);
+    noFill();
     drawGoat(width / 2, height / 2);
-    for (let i = 0; i < reds.length; i++) {
-      let r = reds[i];
-      fill(redShade);
-      ellipse(x, y, w, w);
-      x += speedX;
-      y += speedY;
-      w -= speedW;
-      if (w <= 0) {
-        reds.splice(i, 1);
-        i--;
-      }
+    push();
+  for (let i = 0; i < 10; i++) {
+    let r = i * 10;
+    stroke(255, 0, 0);
+    ellipse(width/2, height/2, diam + r);
+    
+    rotate(sin(frameCount + i) * 100);
+    wave = map(sin(frameCount), -1, 1, 50, 255);
+    stroke(wave);
+    
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    ellipse(width/2, height/2, diam + r);
+    
+    rotate(sin(frameCount + i) * 100);
+    wave = map(cos(frameCount), -1, 1, 50, 255);
+    stroke(wave);
+    
+    
+    stroke(255, 0, 0);
+    ellipse(random(width, height), random(width, height), diam + r);
+    
+  }
+  
+  diam += 10;
+  
+    if (diam > 500) {
+    diam = 0;
+  }
+  pop();
+  
+  }
+  push();
+  text(timer, width/2, height * 0.9);
+  pop();
+  
+  if (frameCount % 60 == 0 && timer > 0) { 
+    timer --;
+  }
+  if (timer == 0) {
+    background(159, 189, 237);
+  
+    
+  // sky
+  for(let i=0; i<=height; i++){
+    stroke(255-i,255-i,255);
+    line(0,200-i,width,200-i);
+  }
+  
+  // sun
+  noStroke();
+  fill(250,186, 3);
+  ellipse(200,100, 70, 70);
+  
+  // mountains
+  fill(77, 61, 18);
+  triangle(38, 208, 102, 63, 145, 200);
+  triangle(175, 208, 205, 105, 250, 200);
+  triangle(235, 208, 270, 63, 290, 200);
+  triangle(300, 208, 320, 105, 340, 200);  
+    
+  // cloud
+    for(let c=0; c<6; c++){
+    fill(255, 255, 255);
+    ellipse(50 + (5 * c), 100, 40 + c, 20);
+    ellipse(100 + (4 * c), 120, 40 + c, 20);
+    ellipse(175 + (5 * c), 110, 40 + c, 20);
+    ellipse(432 + (5 * c), 110, 40 + c, 20);
     }
-    if (frameCount % 10 == 0) {
-      let r = {};
-      x = random(width);
-      y = random(height);
-      w = random(50, 100);
-      redShade = color(random(200, 255), 0, 0);
-      speedX = random(-5, 5);
-      speedY = random(-5, 5);
-      speedW = random(1, 3);
-      reds.push(r);
-    }
+    
+  // pasture
+  noStroke();
+  fill(0,100,0);
+  rect(0,200,width,200);
+  
+  // trees
+  fill(67, 51, 8); 
+  rect(320,194,10,20); 
+  fill(200, 255, 100);
+  ellipse(326,172,54,20);
+  ellipse(324,192,30,20);
+  ellipse(330,182,30,20);
+
+  push();
+    translate(-40,-40);
+    scale(1.5);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326, 192, 15, 20);
+    ellipse(324,172,30,20);
+    ellipse(330,182,20,20);
+  pop();
+    
+  push();
+    translate(-80,-80);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,20,20);
+    ellipse(324,172,30,20);
+    ellipse(330,182,20,20);
+  pop();
+    
+  push();
+    translate(-400,-120);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,16,20);
+    ellipse(324,172,25,20);
+    ellipse(330,182,8,20);
+  pop();
+    
+  push();
+    translate(-550,-80);
+    scale(2);
+    fill(67, 51, 8); 
+    rect(320,194,10,20); 
+    fill(100, 255, 100);
+    ellipse(326,192,20,20);
+    ellipse(324,172,15,20);
+    ellipse(330,182,15,20);
+  pop();
+    
+    drawGoat(a, b);
+    text('Congratulations, you made it!', width/2, height*0.12)
   }
 }
+
 
 function drawGoat(x, y) {
   push();
