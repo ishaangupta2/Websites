e053ce71d936aeefb0dd17f01db6933651475b23
ishaangupta2 <143498568+ishaangupta2@users.noreply.github.com>
2023-11-30 23:47:28 +0800

    Project B Draft Modifications

---
+++ project-b-draft/Minisites/firstCanvas/sketch.js
@@ -0,0 +1,27 @@
+console.log("js is linked!");
+
+let carSound;
+let backgroundImage;
+
+function preload(){
+  carSound = loadSound("speedingCar.mp3");
+  backgroundImage = loadImage("Picture1.png");
+
+}
+
+function setup() {
+  let cnv = createCanvas(400,235);
+  cnv.parent("canvasWrapper");
+
+}
+
+function draw() {
+  background(0);
+  image(backgroundImage, -10, -10, 430, 260);
+
+}
+
+function pressedSpeeding(){
+  carSound.play();
+
+}
